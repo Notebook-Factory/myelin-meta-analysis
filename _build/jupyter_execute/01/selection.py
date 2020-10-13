7a897355e0ba6e3968ff633afde74ce125f62bb1
Agah Karakuzu <agahkarakuzu@gmail.com>
2020-10-13 10:08:36 -0400

    Add new book

---
+++ _build/jupyter_execute/01/selection.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Literature overview
+# 
+# First, how were the studies selected? We used the [Medline database](https://pubmed.ncbi.nlm.nih.gov) and retrieved all the records mentioning (1) myelin, (2) MRI and (3) histology (or a related technique). The full list of keywords is provided in the preprint.
+# 
+# 
+# ```{admonition} Figure 1
+# :class: tip
+# The Sankey diagram shows the screening procdess. You can hover with the mouse on each block and connection to see details about the number of studies and exclusion criteria.
+# ```
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+import plotly.graph_objects as go
+from IPython.core.display import display, HTML
+from plotly.offline import plot
+import plotly.express as px
+import plotly.colors
+from plotly.subplots import make_subplots
+
+from rpy2.robjects.packages import importr
+import rpy2.robjects
+
+
+# ### Figure 1
+
+# In[2]:
+
+
+config={'showLink': False, 'displayModeBar': False}
+
+screening_info = ['Records obtained from the Medline database',
+                  'Records obtained from previous reviews',
+                  """
+                    Exclusion critera:<br>
+                    - work relying only on MRI;<br>
+                    - work relying only on histology or equivalent approach;<br>
+                    - work reporting only qualitative comparisons.
+                  """,
+                  'Records selected for full-text evaluation',
+                  """
+                    Exclusion criteria:<br>
+                    - studies using MRI-based measures in arbitrary units;<br>
+                    - studies using measures of variation in myelin content;<br>
+                    - studies using arbitrary assessment scales;<br>
+                    - studies comparing absolute measures of myelin with relative measures;<br>
+                    - studies reporting other quantitative measures than correlation or R^2 values;<br>
+                    - studies comparing histology from one dataset and MRI from a different one.
+                  """,
+                  'Studies selected for literature overview',
+                  """
+                    Exclusion criteria:<br>
+                     - not providing an indication of both number of subjects and number of ROIs.
+                  """]
+
+fig1 = go.Figure(data=[go.Sankey(
+    arrangement = "freeform",
+    node = dict(
+      pad = 80,
+      thickness = 10,
+      line = dict(color = "black", width = 0.5),
+      label = ["Main records identified (database searching)",
+               "Additional records (reviews)",
+               "Records screened",
+               "Records excluded",
+               "Full-text articles assessed for eligibility",
+               "Full-text articles excluded",
+               "Studied included in the literature overview",
+               "Studies included in the meta-analysis"],
+      x = [0, 0, 0.4, 0.6, 0.5, 0.8, 0.7, 1],
+      y = [0, 0, 0.5, 0.8, 0.15, 0.05, 0.4, 0.6],
+      hovertemplate = "%{label}<extra>%{value}</extra>",
+      color = ["darkblue","darkblue","darkblue","darkred","darkgreen","darkred","darkgreen","darkgreen"]
+    ),
+    link = dict(
+      source = [0, 1, 2, 2, 4, 4, 6],
+      target = [2, 2, 3, 4, 5, 6, 7],
+      value = [688, 1, 597, 92, 34, 58, 43],
+      customdata = screening_info,
+      hovertemplate = "%{customdata}",
+  ))])
+
+fig1.update_layout(title = dict(text="Figure 1 - Review methodology"),
+                   width=650,
+                   height=450,
+                   font_size=10,
+                  margin=dict(l=0))
+
+plot(fig1, filename = 'fig1.html',config = config)
+display(HTML('fig1.html'))
+
+
+# We identified 58 studies reporting quantitative comparisons between MRI and histology: these included a variety of methodological choices and experimental conditions, in terms of tissue type (brain, spinal cord, peripheral nerve), condition (*in vivo*, *ex vivo*, *in situ*), species (human, animal), pathology model, and many more. A glimpse of these subdivisions is provided in the following treemap.
+# 
+# ```{admonition} Figure 2
+# :class: tip
+# You can click on each box to expand the related category, and for each study you can find out more details and the link to the original paper.
+# ```
+
+# ### Figure 2
+
+# In[4]:
+
+
+info = pd.read_excel('database.xlsx', sheet_name='Details')
+
+year_str = info['Year'].astype(str)
+info['Study'] = info['First author'] + ' et al., ' + year_str
+info['Study'] = info.groupby('Study')['Study'].apply(lambda n: n+list(map(chr,np.arange(len(n))+97))
+                                                     if len(n)>1 else n)
+info['Number of studies'] = np.ones((len(info),1))
+info = info.sort_values('Study')
+
+info['Link'] = info['DOI']
+info['Link'].replace('http',"""<a style='color:white' href='http""",
+                    inplace=True, regex=True)
+info['Link'] = info['Link'] + """'>->Go to the paper</a>"""
+
+fields = ['Approach', 'Magnetic field', 'MRI measure(s)',
+          'Histology/microscopy measure', 'Specific structure(s)']
+info['Summary'] = info['Link'] + '<br><br>'
+for i in fields:
+    info['Summary'] = info['Summary'] + i + ': ' + info[i].astype(str) + '<br><br>'
+
+args = dict(data_frame=info, values='Number of studies',
+            color='Number of studies', hover_data='',
+            path=['Focus', 'Tissue condition', 'Human/animal', 'Condition', 'Study'],
+            color_continuous_scale='Viridis')
+args = px._core.build_dataframe(args, go.Treemap)
+treemap_df = px._core.process_dataframe_hierarchy(args)['data_frame']
+
+fig2 = go.Figure(go.Treemap(
+        ids=treemap_df['id'].tolist(),
+        labels=treemap_df['labels'].tolist(),
+        parents=treemap_df['parent'].tolist(),
+        values=treemap_df['Number of studies'].tolist(),
+        branchvalues='total',
+        text=info['Summary'],
+        hoverinfo='label',
+        textfont=dict(
+            size=15,
+        )
+    )
+)
+
+fig2 = fig2.update_layout(
+    autosize=False,
+    width=680,
+    height=600,
+    margin=dict(
+        l=0,
+        r=0,
+        t=0,
+        b=0,
+    )
+)
+
+plot(fig2, filename = 'fig2.html',config = config)
+display(HTML('fig2.html'))
+
---
+++ _build/jupyter_execute/04/other_factors.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Potential confounders
+# Can some of this variance be explained by the differences in methodological choices and experimental conditions we mention? The number of studies is limited for a quantitative evaluation, but we can get a qualitative idea using bar plots and scatter plots organized by each condition.
+# 
+# ```{admonition} Figure 7
+# :class: tip
+# Distributions of R<sup>2</sup> values in relation to the reference techniques, pathology and tissue types are visualized using box plots. You can mouse over markers to see the studies they are drawn from.
+# ```
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+import plotly.graph_objects as go
+from IPython.core.display import display, HTML
+from plotly.offline import plot
+import plotly.express as px
+import plotly.colors
+from plotly.subplots import make_subplots
+
+from rpy2.robjects.packages import importr
+import rpy2.robjects
+
+
+# ### Figure 7
+
+# In[2]:
+
+
+config={'showLink': False, 'displayModeBar': False}
+
+metafor = importr('metafor')
+stats = importr('stats')
+
+filtered_df = pd.read_pickle("filtered_df.pkl")
+
+filtered_df['Variance'] = (4*filtered_df['R^2'])*((1-filtered_df['R^2'])**2)/filtered_df['Sample points']
+
+structures={'Lesions':'Lesions',
+            'Substantia nigra':'Deep grey matter',
+            'Hippocampal commissure':'White matter',
+            'Putamen':'Deep grey matter',
+            'Motor cortex':'Grey matter',
+            'Globus pallidus':'Deep grey matter',
+            'Perforant pathway':'White matter',
+            'Mammilothalamic tract':'White matter',
+            'External capsule':'White matter',
+            'Inter-peduncular nuclues':'Deep grey matter',
+            'Hippocampus':'Deep grey matter',
+            'Thalamic nuclei':'Deep grey matter',
+            'Thalamus':'Deep grey matter',
+            'Cerebellum':'Grey matter',
+            'Amygdala':'Deep grey matter',
+            'Cingulum':'White matter',
+            'Striatum':'Deep grey matter',
+            'Accumbens':'Deep grey matter',
+            'Basal ganglia':'Deep grey matter',
+            'Anterior commissure':'White matter',
+            'Cortex':'Grey matter',
+            'Fimbria':'White matter',
+            'Somatosensory cortex':'Grey matter',
+            'Dorsal tegmental tract':'White matter',
+            'Superior colliculus':'Deep grey matter',
+            'Fasciculus retroflexus':'White matter',
+            'Optic nerve':'White matter',
+            'Dentate gyrus':'Grey matter',
+            'Corpus callosum':'White matter',
+            'Fornix':'White matter',
+            'White matter':'White matter',
+            'Grey matter':'Grey matter',
+            'Optic tract':'White matter',
+            'Internal capsule':'White matter',
+            'Stria medullaris':'White matter'}
+
+
+tissue_types=[]
+for s in filtered_df['Specific structure(s)']:
+    t_list=[]
+    for i in s.split(','):
+        t_list.append(structures[i.strip()])
+    tissue_types.append('+'.join(list(set(t_list))))
+
+filtered_df['Tissue types']=tissue_types
+
+fig7 = make_subplots(rows=3, cols=1, start_cell="top-left", vertical_spacing=0.2, y_title='R<sup>2</sup>',
+                     subplot_titles=['R<sup>2</sup> values and reference techniques',
+                                     'R<sup>2</sup> values and pathology',
+                                     'R<sup>2</sup> values and tissue types'])
+
+references = ['Histology', 'Immunohistochemistry', 'Microscopy', 'EM']
+
+for r in references:
+    df_r=filtered_df[filtered_df['Histology/microscopy measure'].str.contains(r)]
+    fig7.add_trace(go.Box(
+        y=df_r['R^2'],
+        x=df_r['Histology/microscopy measure'],
+        boxpoints='all',
+        text=df_r['Measure'] + ' - ' + df_r['Study'],
+        name=r
+    ), col=1, row=1)
+
+for t in filtered_df['Condition'].unique():
+    df_t=filtered_df[filtered_df['Condition']==t]
+    fig7.add_trace(go.Box(
+        y=df_t['R^2'],
+        x=df_t['Condition'],
+        boxpoints='all',
+        text=df_t['Measure'] + ' - ' + df_t['Study'],
+        name=t
+    ), col=1, row=2)
+
+for t in filtered_df['Tissue types'].unique():
+    df_t=filtered_df[filtered_df['Tissue types']==t]
+    fig7.add_trace(go.Box(
+        y=df_t['R^2'],
+        x=df_t['Tissue types'],
+        boxpoints='all',
+        text=df_t['Measure'] + ' - ' + df_t['Study'],
+        name=t
+    ), col=1, row=3)   
+
+fig7.update_layout(
+    title=dict(
+        text='Figure 7: Experimental conditions and methodological choices influencing the R<sup>2</sup> values',
+        x=0),
+    margin=dict(l=0),
+    showlegend=False,
+    height=1000,
+    width=600
+)
+
+fig7.update_xaxes(tickfont=dict(size=10))
+plot(fig7, filename = 'fig7.html',config = config)
+display(HTML('fig7.html'))
+
+
+# 
+# ```{admonition} Figure 8
+# :class: tip
+# Other potential confounders include magnetic field strength, tissue conditions, co-registration and tissue type. You can mouse over markers in each panel to see the corresponding studies.
+# ```
+# 
+# ### Figure 8
+
+# In[3]:
+
+
+measure_type = {'Diffusion':['RD', 'AD', 'FA', 'MD',
+                'AWF', 'RK', 'RDe', 'MK'],
+                'Magnetization transfer':['MTR',
+                'ihMTR', 'MTR-UTE', 'MPF', 'MVF-MT',
+                'R1f', 'T2m', 'T2f', 'k_mf','k_fm'],
+                'T1 relaxometry':['T1'], 'T2 relaxometry':['T2', 'MWF', 'MVF-T2'],
+                'Other':['QSM', 'R2*', 'rSPF', 'MTV',
+                'T1p', 'T2p', 'RAFF', 'PD', 'T1sat']}
+
+color_dict = {m:plotly.colors.qualitative.Bold[n]
+              for n,m in enumerate(measure_type.keys())}
+
+fig8 = make_subplots(rows=2, cols=2, start_cell="top-left", vertical_spacing=0.18, y_title='R<sup>2</sup>',
+                     subplot_titles=['R<sup>2</sup> values and magnetic fields',
+                                     'R<sup>2</sup> values and tissue conditions',
+                                     'R<sup>2</sup> values and co-registration',
+                                     'R<sup>2</sup> values and human/animal tissue'
+                                    ])
+
+for m in measure_type.keys():
+    df_m=filtered_df[filtered_df["Measure"].isin(measure_type[m])]
+    fig8.add_trace(go.Scatter(x=df_m['Magnetic field'],
+                              y=df_m['R^2'],
+                              text=df_m['Measure'] + ' - ' + df_m['Study'],
+                              marker=dict(color=color_dict[m]),
+                              name=m,
+                              mode='markers'), col=1, row=1)
+
+fig8.update_layout(
+    xaxis=dict(title='Magnetic field [T]')
+)
+
+for t in filtered_df['Tissue condition'].unique():
+    df_t=filtered_df[filtered_df['Tissue condition']==t]
+    fig8.add_trace(go.Box(
+        y=df_t['R^2'],
+        x=df_t['Tissue condition'],
+        boxpoints='all',
+        text=df_t['Measure'] + ' - ' + df_t['Study'],
+        name=t
+    ), col=2, row=1)
+    
+for t in filtered_df['Co-registration'].unique():
+    df_t=filtered_df[filtered_df['Co-registration']==t]
+    fig8.add_trace(go.Box(
+        y=df_t['R^2'],
+        x=df_t['Co-registration'],
+        boxpoints='all',
+        text=df_t['Measure'] + ' - ' + df_t['Study'],
+        name=t
+    ), col=1, row=2)
+    
+for t in filtered_df['Human/animal'].unique():
+    df_t=filtered_df[filtered_df['Human/animal']==t]
+    fig8.add_trace(go.Box(
+        y=df_t['R^2'],
+        x=df_t['Human/animal'],
+        boxpoints='all',
+        text=df_t['Measure'] + ' - ' + df_t['Study'],
+        name=t
+    ), col=2, row=2)
+
+fig8.update_layout(
+    title=dict(text='Figure 8: Other factors to consider when assessing R<sup>2</sup>', x=0.1),
+    margin=dict(l=100),
+    showlegend=False,
+    height=600,
+    width=700
+)    
+    
+plot(fig8, filename = 'fig8.html',config = config)
+display(HTML('fig8.html'))
+
